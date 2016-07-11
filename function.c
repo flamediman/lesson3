76d82c16bc60608c637422775a1e6e970c950b90
Anastasia Kultysheva <kasandrocka.ru@mail.ru>
2016-07-11 18:23:50 +0300

    new function.c

--- function.c
+++ function.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "digital_array.c"
+#include "power_array.c"
 
 const int N = 10;
 
@@ -7,6 +8,7 @@ int main() {
   int array[N];
 
   input_digital_array(array, N);
+  power_array(array, N);
   output_digital_array(array, N);
 
   return 0;
